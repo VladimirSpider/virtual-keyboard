5cf030bd1d572cc528a7e9168280cdddb3c90d02
Vladimir <vladimir.bur@overdrive.sg>
2023-04-29 00:08:39 +0300

    feat: add some tags

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
-function sum(x, y) {
-  return x + y;
-}
-const x = 100;
-const y = "200";
-const xPlusY = sum(x, y);
-console.log(`${x} + ${y} = ${xPlusY}`);
+document.addEventListener('DOMContentLoaded', function() {
+	const body = document.querySelector('body');
+
+	const createTag = (tag, className, node, text) => {
+		let element = document.createElement(tag);
+		element.classList.add(className);
+		
+		if (text) {
+			element.innerHTML = text;
+		}
+		
+		node.append(element);
+	}
+	
+	createTag('div', 'wrapper', body);
+	const wrapper = document.querySelector('.wrapper');
+	
+	createTag('p', 'title', wrapper, 'Виртуальная клавиатура!');
+	createTag('textarea', 'paper', wrapper);
+	createTag('div', 'keyboard-block', wrapper);
+	const keyboardBlock = document.querySelector('.keyboard-block');
+	
+	
+	createTag('div', 'keyboard', keyboardBlock);
+	createTag('p', 'support', keyboardBlock, 'Клавиатура создана в операционной системе Windows');
+	createTag('p', 'support', keyboardBlock, 'Для переключения языка комбинация: левыe ctrl + alt');
+	
+});
