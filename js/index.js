03e2268ec54ab32cd1fe20ddf975deb9c211d31a
Vladimir <vladimir.bur@overdrive.sg>
2023-04-30 00:31:52 +0300

    feat: add english keyboard

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+const buttonsEng = [
+	[['`', '~'], ['1', '!'], ['2', '@'], ['3', '#'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('], ['0', ')'], ['-', '_'], ['=', '+'], ['Backspace', ['backspace']]],
+	[['Tab', ['tab']], ['q', 'Q'], ['w', 'W'], ['e', 'E'], ['r', 'R'], ['t', 'T'], ['y', 'Y'], ['u', 'U'], ['i', 'I'], ['o', 'O'], ['p', 'P'], ['[', '{'], [']', '}'], ['\\', '|'], ['Del', ['del']]],
+	[['CapsLock', ['capslock']], ['a', 'A'], ['s', 'S'], ['d', 'D'], ['f', 'F'], ['g', 'G'], ['h', 'H'], ['j', 'J'], ['k', 'K'], ['l', 'L'], [';', ':'], ["'", '"'], ['Enter', ['enter']]],
+	[['Shift', ['shift']], ['z', 'Z'], ['x', 'X'], ['c', 'C'], ['v', 'V'], ['b', 'B'], ['n', 'N'], ['m', 'M'], [',', '<'], ['.', '>'], ['/', '?'], ['▲', ['up']], ['Shift', ['shift']]],
+	[['Ctrl', ['ctrl-left']], ['Win', ['win']], ['Alt', ['alt']], ['', ['space']], ['Alt', ['alt']], ['◄', ['left']], ['▼', ['down']], ['►', ['right']], ['Ctrl', ['ctrl-right']]]
+];
+
 document.addEventListener('DOMContentLoaded', function() {
 	const body = document.querySelector('body');
 
@@ -22,7 +30,34 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	
 	createTag('div', 'keyboard', keyboardBlock);
-	createTag('p', 'support', keyboardBlock, 'Клавиатура создана в операционной системе Windows');
-	createTag('p', 'support', keyboardBlock, 'Для переключения языка комбинация: левыe ctrl + alt');
+	createTag('p', 'support', wrapper, 'Клавиатура создана в операционной системе Windows');
+	createTag('p', 'support', wrapper, 'Для переключения языка комбинация: левыe ctrl + alt');
+	
+	const keyboard = document.querySelector('.keyboard');
+	
+	buttonsEng.forEach((line, index) => {
+		createTag('div', 'line', keyboard);
+		const buttonsLine = document.querySelectorAll('.line')[index];
+		line.forEach((button) => {
+			if (Array.isArray(button[button.length - 1])) {
+				let div = document.createElement('div');
+				div.classList.add('button-block', button[button.length - 1]);
+				let span = document.createElement('span');
+				span.innerHTML = button[0];
+				div.append(span);
+				buttonsLine.append(div);
+			} else {
+				let div = document.createElement('div');
+				div.classList.add('button-block');
+				button.forEach((value, index) => {
+					let span = document.createElement('span');
+					if (index === 1) span.classList.add('hide-value');
+					span.innerHTML = button[index];
+					div.append(span);
+				});
+				buttonsLine.append(div);
+			}
+		});
+	});
 	
 });
